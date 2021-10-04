9878075ea734ffa8da95d5d19a2836bb5f60d0ff
HAFDIAHMED <ahafdi62@gmail.com>
2021-10-04 13:58:30 +0100

    counter

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+ import { useState } from 'react';
+ import React from 'react';
+ import { StatusBar } from 'expo-status-bar';
+ import { StyleSheet, Text, View, Button ,TextInput} from 'react-native';
 
-export default function App() {
+function Example() {
+  // Declaración de una variable de estado que llamaremos "count"
+  const [count, setCount] = useState(0);
+  const [age,setAge]=useState(11);
+  function addCout (){
+    return (
+      setCount(count +1)
+    );
+   
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>You clicked {count} times</Text>
+
+      <Button 
+      title="Click me"
+      onPress={()=>addCout()}
+     
+      
+       />
+       <Text> enter your age here : </Text>
+       
+       <Text>You are {age} years old</Text>
+
+        
+      
     </View>
   );
 }
-
+export default Example;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f7f7f7',
+
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
